9cc92820b829448eaf9a3d0336c015e0ac05e008
fedya <myEmail>
2022-12-05 11:45:22 +0300

    made tasks and ability to delete them and doubleclick on them

--- src/App.js
+++ src/App.js
@@ -1,12 +1,49 @@
+import { useState } from "react"
+
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+
+
+
+
+
 function App() {
-  return (
-    <div className="container">
-      <Header/>
-      <Tasks/>
-    </div>
-  );
+    const [tasks, setTasks] = useState([
+        {
+            id: 1,
+            text: "Guitar lessons",
+            day: "Dec 5th at 3:00pm",
+            reminder: true,
+        },
+        {
+            id: 2,
+            text: "Dentist appointment",
+            day: "Dec 15th at 6:00pm",
+            reminder: true,
+        },
+        {
+            id: 3,
+            text: "Date",
+            day: "Dec 30th at 23:00pm",
+            reminder: false,
+        }
+    ])
+
+    const deleteTask = (id) => {
+        setTasks(tasks.filter((task)=>task.id!==id))
+    }
+
+
+    const toggleReminder = (id) => {
+        console.log(id)
+    }
+
+    return (
+        <div className="container">
+            <Header />
+            {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : "No tasks to show!"}
+        </div>
+    );
 }
 
 export default App;
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,35 +1,13 @@
-import {useState} from "react"
+import Task from "./Task"
 
-const tasks = [
-    {
-        id: 1,
-        text: "Guitar lessons",
-        day: "Dec 5th at 3:00pm",
-        reminder: true,
-    },
-    {
-        id: 2,
-        text: "Dentist appointment",
-        day: "Dec 15th at 6:00pm",
-        reminder: true,
-    },
-    {
-        id: 3,
-        text: "Date",
-        day: "Dec 30th at 23:00pm",
-        reminder: false,
-    }
-]
-const t = tasks.map((task) => (
-    <h3 key={task.id}>{task.text}</h3>
-))
 
-const Tasks = () => {
-  return (
-    <>
-        {t}
-    </>
-  )
+const Tasks = ({tasks, onDelete, onToggle}) => {
+    
+    return (
+        <>
+            {tasks.map((task) => <Task key={task.id} task={task} onDelete={onDelete} onToggle={onToggle}/>)}
+        </>
+    )
 }
 
 export default Tasks
